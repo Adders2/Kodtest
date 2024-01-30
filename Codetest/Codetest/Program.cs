bdfca7e1ab5bbb5ee9fae27f80020f6369a3d138
Adders2 <osterlund.adam@hotmail.com>
2024-01-30 19:08:38 +0100

    trying xdoc

--- Codetest/Codetest/Program.cs
+++ Codetest/Codetest/Program.cs
@@ -1,8 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
-using Codetest.Model;
-using System.Text;
+using System.Xml.Linq;
 
 internal class Program
 {
@@ -16,9 +14,34 @@ internal class Program
 
         var splitRows = text.Select(t => t.Split('|')).ToList();
 
+        var xdoc = new XDocument(new XElement("people"));
+
         foreach (var splitRow in splitRows)
         {
-            
+            var key = splitRow[0];
+
+            switch (key)
+            {
+                case "P":
+                    xdoc.Root.Add(new XElement("person"));
+                    break;
+                case "T":
+                    xdoc.Root.Element("person").Add(new XElement("phone"));
+                    break;
+                case "A":
+                    xdoc.Root.Element("person").Add(new XElement("address"));
+                    break;
+                case "F":
+                    xdoc.Root.Element("person").Add(new XElement("family"));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        foreach (var thing in xdoc.Elements())
+        {
+            Console.WriteLine(thing);
         }
         
 
