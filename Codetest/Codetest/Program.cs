6dd35fdae7f91310d9305a6c7154cdbc3e08adcc
Adders2 <osterlund.adam@hotmail.com>
2024-01-30 18:59:01 +0100

    reading file, not sure wether to use models or go straight from text to xml...

--- Codetest/Codetest/Program.cs
+++ Codetest/Codetest/Program.cs
@@ -2,11 +2,26 @@
 
 
 using Codetest.Model;
+using System.Text;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        var currentDir = AppDomain.CurrentDomain.BaseDirectory;
+        var file = Path.Combine(currentDir, "../../../Example.txt");
+        var path = Path.GetFullPath(file);
+
+        var text = File.ReadAllLines(path);
+
+        var splitRows = text.Select(t => t.Split('|')).ToList();
+
+        foreach (var splitRow in splitRows)
+        {
+            
+        }
         
+
+        Console.ReadLine();
     }
 }
